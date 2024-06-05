964cec7aa829d66a650ea37cd75a407fa8dd9c0a
Aayush701 <ayushghimire70111@gmail.com>
2024-06-05 18:27:39 +0545

    fetch lecture contorller completed

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,5 +1,6 @@
 import TryCatch from "../middleware/TryCatch.js";
 import { Courses } from "../models/coursesModel";
+import { User } from "../models/userModel.js";
 
 export const getAllCourse = TryCatch(async (req, res) => {
   const course = await Courses.find();
@@ -23,4 +24,16 @@ export const getSingleCourse = TryCatch(async (req, res) => {
   }
 });
 
-export const fetchLectures = TryCatch(async (req, res) => {});
+export const fetchLectures = TryCatch(async (req, res) => {
+  const user = await User.findById(req.user_id);
+
+  if (user.role === "admin") {
+    return res.json({ lectures });
+  }
+
+  if (!user.subscription.includes(req.params.id)) {
+    return res.status(400).json({
+      message: "You have not subscribed to this course",
+    });
+  }
+});
--- backend/routes/coursesRoute.js
+++ backend/routes/coursesRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllCourse, getSingleCourse } from "../controllers/courseController";
+import {
+  fetchLectures,
+  getAllCourse,
+  getSingleCourse,
+} from "../controllers/courseController";
+import { Auth } from "../middleware/auth";
 
 const router = express.Router();
 
 router.get("/course/all", getAllCourse);
 router.get("/course/:id", getSingleCourse);
+router.get("/lectures/:id", Auth, fetchLectures);
 
 export default router;
